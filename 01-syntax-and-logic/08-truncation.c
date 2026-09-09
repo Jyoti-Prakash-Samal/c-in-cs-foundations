836ee32789ca7e6a6a03aaecdbeacae7deea132b
Jyoti-Prakash-Samal <jyotiprakash6911@gmail.com>
2026-09-09 17:55:09 +0530

    Fixed fatal errors in last two programs

--- 01-syntax-and-logic/08-truncation.c
+++ 01-syntax-and-logic/08-truncation.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main(void){
     int p,q,r,sum;
-    printf("Enter the scores of three subjects\n")
-    scanf("%d","%d","%d", &p,&q,&r);
+    printf("Enter the scores of three subjects\n");
+    scanf("%d %d %d", &p,&q,&r);
     sum=p+q+r;
-    float avg= sum/3;
-    printf("The average upto 2 decimals is: %.2f\n");
+    float avg= sum/3.0;
+    printf("The average upto 2 decimals is: %.2f\n", avg);
     return 0;
 }
--- 01-syntax-and-logic/09-do_while.c
+++ 01-syntax-and-logic/09-do_while.c
@@ -5,8 +5,8 @@ int main(void)
     do
     {
         printf("Enter the amount to be withdrawn, Note: The amount should be positive and divisible by 100\n");
-scanf("&d" , &p);
-    } while(p<0 || p%100!=0)
-    printf("Dispensing: %d" , &p);
+scanf("%d" , &p);
+    } while(p<=0 || p%100!=0);
+    printf("Dispensing: %d" , p);
     return 0;
 }
