bb568649f9330e1408d03c671183ec6a27d7d5c5
Jyoti-Prakash-Samal <jyotiprakash6911@gmail.com>
2026-09-10 23:54:18 +0530

    Learnt about bitwise

---
+++ 01-syntax-and-logic/10-bitwise.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(vvoid){
+    int n;
+    printf("Enter a number\n");
+    scanf("%d", &n);
+    if(&n ==1)
+    {
+        printf("The number is odd\n");
+    }
+    else{
+        printf("the nummber is even");
+    }
+    return 0;
+}
